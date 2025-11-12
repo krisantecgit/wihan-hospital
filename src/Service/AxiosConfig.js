07dab805e454c2ca20603d653dd7b20999ae7cd0
dharavath <daravathnagamani50@gmail.com>
2025-11-12 16:14:00 +0530

    hospital profile added

--- src/Service/AxiosConfig.js
+++ src/Service/AxiosConfig.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // Create an Axios instance
 const axiosInstance = axios.create({
   // baseURL: "http://192.168.0.241:9876/",
-  baseURL: "https://api-staging.wihan.in/",
-  // baseURL: "https://api.wihan.in/",
+  // baseURL: "https://api-staging.wihan.in/",
+  baseURL: "https://api.wihan.in/",
 });
 
 // Add a request interceptor to attach the token from localStorage
